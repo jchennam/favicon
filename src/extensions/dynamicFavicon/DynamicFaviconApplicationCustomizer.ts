9114f699c108d6dc380f162fcc9898f8df67cf60
Jag Chennam <jag.chennam@coxautoinc.com>
2020-07-28 08:55:56 -0700

    Initial checkin

---
+++ src/extensions/dynamicFavicon/DynamicFaviconApplicationCustomizer.ts
@@ -0,0 +1,39 @@
+import { override } from '@microsoft/decorators';
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer
+} from '@microsoft/sp-application-base';
+import { Dialog } from '@microsoft/sp-dialog';
+
+import * as strings from 'DynamicFaviconApplicationCustomizerStrings';
+
+const LOG_SOURCE: string = 'DynamicFaviconApplicationCustomizer';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IDynamicFaviconApplicationCustomizerProperties {
+  faviconpath: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class DynamicFaviconApplicationCustomizer
+  extends BaseApplicationCustomizer<IDynamicFaviconApplicationCustomizerProperties> {
+
+  @override
+  public onInit(): Promise<void> {
+    let url: string = this.properties.faviconpath;
+    if (!url) {
+      Log.info(LOG_SOURCE, "No Icon URL provided");
+    }else{
+        var link = document.querySelector("link[rel*='icon']") as HTMLElement || document.createElement('link') as HTMLElement;
+        link.setAttribute('type', 'image/x-icon');
+        link.setAttribute('rel', 'shortcut icon');
+        link.setAttribute('href', url);
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+    return Promise.resolve();
+  }
+}
